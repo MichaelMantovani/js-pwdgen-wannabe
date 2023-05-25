2efaa27c3eb5bb4949e712f21b6ac56b8d4ecd61
MichaelMantovani <michmantovani@gmail.com>
2023-05-25 15:46:02 +0200

    Add const userSurname

--- js/script.js
+++ js/script.js
@@ -18,9 +18,17 @@ console.log ('JS OK');
   console.log(passwordPlaceholder);
 
 
-  // Chiedo all'utente il suo nome
+  // Chiedo all'utente il suo nome e creo una variabile con il nome dell'utente
 
   const userName = prompt('Come ti chiami ?' , 'Michael');
   console.log(userName);
+
+  // Chiedo all'utente il suo cognome e creo una variabile con il cognome dell'utente
+
+  const userSurname = prompt('Qual è il tuo cognome ?' , 'Mantovani');
+  console.log(userSurname);
+
+  
+
 
   
