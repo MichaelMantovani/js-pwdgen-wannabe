019a6428280ab0292d183158c63acb900390f07d
MichaelMantovani <michmantovani@gmail.com>
2023-05-25 15:49:19 +0200

    Add const userFavoriteColor

--- js/script.js
+++ js/script.js
@@ -18,17 +18,21 @@ console.log ('JS OK');
   console.log(passwordPlaceholder);
 
 
-  // Chiedo all'utente il suo nome e creo una variabile con il nome dell'utente
+  // Chiedo all'utente il suo nome e creo una variabile con il suo nome
 
   const userName = prompt('Come ti chiami ?' , 'Michael');
   console.log(userName);
 
-  // Chiedo all'utente il suo cognome e creo una variabile con il cognome dell'utente
-
+  // Chiedo all'utente il suo cognome e creo una variabile con il suo cognome
+  
   const userSurname = prompt('Qual è il tuo cognome ?' , 'Mantovani');
   console.log(userSurname);
-
   
+  // Chiedo all'utente il suo colore preferito e creo una variabile con il suo colore preferito
+  
+const userFavoriteColor = prompt('Qual è il tuo colore preferito ?' , 'Viola');
+console.log(userFavoriteColor);
+
 
 
   
